837f0781f171961b1076d3fe99b73cb19a4db266
lubomir-becka <lubomir.beckaml@gmail.com>
2025-06-06 20:31:58 +0200

    UI improvements, timer style and buttons logic

--- app/src/main/java/com/example/timetracker/ui/screens/TimerScreen.kt
+++ app/src/main/java/com/example/timetracker/ui/screens/TimerScreen.kt
@@ -2,16 +2,24 @@ package com.example.timetracker.ui.screens
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Button
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.core.util.TimeUtils.formatDuration
 
 
 import com.example.timetracker.viewmodel.TimerVM
@@ -20,32 +28,38 @@ import com.example.timetracker.viewmodel.TimerVM
 @Composable
 
 fun TimerScreen(timer: TimerVM) {
-    Column(modifier = Modifier.padding(16.dp)) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
         OutlinedTextField(
             value = timer.name,
             onValueChange = { timer.updateName(it) },
-            label = { Text("Názov aktivity") }
+            label = { Text("Názov aktivity") },
+            modifier = Modifier.padding(top = 32.dp).fillMaxWidth()
         )
 
         Text(
-            text = "Trvanie: ${timer.duration} sekúnd"
+            text = formatTime(timer.duration),
+            style = MaterialTheme.typography.displayLarge,
+            modifier = Modifier
+                .padding(top = 32.dp, bottom = 32.dp)
+                .align(Alignment.CenterHorizontally)
         )
-
-        Row {
+        Spacer(modifier = Modifier.height(32.dp))
+        Row (
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ){
             Button(
-                onClick = { timer.start() },
-                enabled = !timer.isRunning
+                onClick = {
+                    if (timer.isRunning) timer.stop() else timer.start()
+                }
             ) {
-                Text("Štart")
+                Text(if (timer.isRunning) "Stop" else "Štart")
             }
 
-            Button(
-                onClick = { timer.stop() },
-                enabled = timer.isRunning
-            ) {
-                Text("Stop")
-            }
-
+            Spacer(modifier = Modifier.width(16.dp))
             Button(
                 onClick = { timer.reset() }
             ) {
@@ -53,4 +67,11 @@ fun TimerScreen(timer: TimerVM) {
             }
         }
     }
+}
+
+fun formatTime(seconds: Long): String {
+    val _hours = seconds / 3600;
+    val _minutes = (seconds % 3600) / 60
+    val _seconds = seconds % 60
+    return "%02d:%02d:%02d".format(_hours , _minutes, _seconds)
 }
--- app/src/main/java/com/example/timetracker/viewmodel/TimerVM.kt
+++ app/src/main/java/com/example/timetracker/viewmodel/TimerVM.kt
@@ -1,37 +1,50 @@
 package com.example.timetracker.viewmodel
 
 import android.os.Build
-import android.os.health.TimerStat
 import androidx.annotation.RequiresApi
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.timetracker.data.ActivityEntry
 import androidx.lifecycle.viewModelScope
 import com.example.timetracker.repositories.ActivityRepository
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import java.time.Instant
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 
+private var timerJob: Job? = null
 @RequiresApi(Build.VERSION_CODES.O)
 class TimerVM(private val repository: ActivityRepository) : ViewModel(){
 
-    var name: String = ""
-    var duration: Long = 0L
-    var start: Instant = Instant.EPOCH
-    var isRunning: Boolean = false
+    var name by mutableStateOf("")
+    var duration by mutableStateOf(0L)
+    var start by mutableStateOf(Instant.EPOCH)
+    var isRunning by mutableStateOf(false)
 
     fun updateName(newName: String) {
         name = newName
     }
 
     fun start() {
-        start = Instant.now()
-        isRunning = true
+        if (!isRunning) {
+            start = Instant.now()
+            isRunning = true
+            timerJob = viewModelScope.launch {
+                while (isRunning) {
+                    duration = Instant.now().epochSecond - start.epochSecond
+                    delay(1000)
+                }
+            }
+        }
     }
 
     fun stop() {
         if (name.isNotBlank() && isRunning) {
             duration = Instant.now().epochSecond - start.epochSecond
             isRunning = false
+            timerJob?.cancel()
             val activity = ActivityEntry(
                 name = name,
                 duration = duration,
@@ -47,5 +60,6 @@ class TimerVM(private val repository: ActivityRepository) : ViewModel(){
         isRunning = false
         duration = 0L
         start = Instant.EPOCH
+        timerJob?.cancel()
     }
 }
